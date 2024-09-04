a4a27a2fbf89793f2bf22364a370ad103598b14e
shervin <shervingouhari@gmail.com>
2024-09-04 05:35:45 +0330

    Create create_access_token utility

--- auth/utils.py
+++ auth/utils.py
@@ -1,13 +1,25 @@
-from motor.motor_asyncio import AsyncIOMotorDatabase
+from datetime import datetime, timedelta, timezone
 
+from fastapi.security import OAuth2PasswordRequestForm
+from motor.motor_asyncio import AsyncIOMotorDatabase
+import jwt
+
+from core.settings import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_ACCESS_TOKEN_EXPIRY
 from core.database_utils import get_or_fail
 from core.exceptions import AuthenticationFailedError
 from core.hash import is_valid_password
 
 
-async def authenticate(collection: str, user_1: dict, db: AsyncIOMotorDatabase) -> dict:
-    user_2 = await get_or_fail(collection, {"username": user_1["username"]}, db, AuthenticationFailedError)
-    if not is_valid_password(user_1["password"], user_2["password"]):
+async def authenticate(collection: str, user_1: OAuth2PasswordRequestForm, db: AsyncIOMotorDatabase) -> dict:
+    user_2 = await get_or_fail(collection, {"username": user_1.username}, db, AuthenticationFailedError)
+    if not is_valid_password(user_1.password, user_2["password"]):
         raise AuthenticationFailedError
     else:
         return user_2
+
+
+def create_access_token(user: dict):
+    exp = datetime.now(timezone.utc) + timedelta(seconds=JWT_ACCESS_TOKEN_EXPIRY)
+    user.update({"exp": exp})
+    encoded_jwt = jwt.encode(user, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return encoded_jwt

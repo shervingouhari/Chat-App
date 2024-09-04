595a88be4f3ae2b8846970a6aa21eae543b023a8
shervin <shervingouhari@gmail.com>
2024-09-04 23:23:19 +0330

    Add WWW-Authenticate header to AuthenticationFailedError

--- core/exceptions.py
+++ core/exceptions.py
@@ -7,11 +7,13 @@ class ChatAppAPIError(HTTPException):
     """The base class for all ChatApp exceptions."""
 
     default_status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_headers = None
     default_detail = "An unexpected error occurred."
     default_resolution = "Try again later or report the bug."
 
-    def __init__(self, status_code: int = None, detail: str = None, resolution: str = None):
+    def __init__(self, status_code: int = None, headers: dict = None, detail: str = None, resolution: str = None):
         self.status_code = status_code or self.default_status_code
+        self.headers = headers or self.default_headers
         self.detail = detail or self.default_detail
         self.resolution = resolution or self.default_resolution
         log.error(
@@ -19,6 +21,7 @@ class ChatAppAPIError(HTTPException):
         )
         super().__init__(
             status_code=self.status_code,
+            headers=self.headers,
             detail={
                 "type": self.__class__.__name__,
                 "message": self.detail,
@@ -46,5 +49,9 @@ class AuthenticationFailedError(ChatAppAPIError):
     """Raised when user authentication fails due to incorrect credentials."""
 
     default_status_code = status.HTTP_401_UNAUTHORIZED
+    default_headers = {"WWW-Authenticate": "Bearer"}
     default_detail = "Authentication failed."
-    default_resolution = "Ensure that your username and password are correct and try again."
+    default_resolution = (
+        "Ensure that your username and password are correct. "
+        "If using a token, ensure it is valid and not expired."
+    )

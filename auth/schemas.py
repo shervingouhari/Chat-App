1fb4d09e193fad183dab101f47c718919f49c149
shervin <shervingouhari@gmail.com>
2024-09-04 23:58:42 +0330

    Add TokenPayload

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from core.settings import JWT_TOKEN_TYPE
 
 
 class Token(BaseModel):
-    access_token: str = ...
-    token_type: str = JWT_TOKEN_TYPE
+    access_token: str = Field(...)
+    token_type: str = Field(JWT_TOKEN_TYPE)
+
+
+class TokenPayload(BaseModel):
+    username: str = Field(..., min_length=1)
+    email: str = Field(..., min_length=1)

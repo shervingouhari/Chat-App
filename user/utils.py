4fde3d29119f39354761326c326e99ff774d4984
shervin <shervingouhari@gmail.com>
2024-09-05 04:31:40 +0330

    Create ensure_authority decorator

--- user/utils.py
+++ user/utils.py
@@ -1,9 +1,11 @@
 from typing import Annotated
+from functools import wraps
 
 from fastapi import Path, Body, Depends
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from core.dependencies import get_db
+from core.exceptions import ActionForbiddenError
 from auth.dependencies import get_user
 from .schemas import UserCreate, UserUpdate, ReadUsersQP
 
@@ -15,3 +17,13 @@ User = Annotated[dict, Depends(get_user)]
 UserCreate = Annotated[UserCreate, Body(...)]
 UserUpdate = Annotated[UserUpdate, Body(...)]
 ReadUsersQP = Annotated[ReadUsersQP, Depends()]
+
+
+def ensure_authority(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        if str(kwargs.get('user')["_id"]) != kwargs.get('object_id'):
+            raise ActionForbiddenError
+        else:
+            return await func(*args, **kwargs)
+    return wrapper
